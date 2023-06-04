36e466f99efce8cb9ffcf0cdca3d37262fbc0655
Bentleyr98 <bentley.rebekah314@gmail.com>
2023-06-03 21:31:13 -0600

    added oauth

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
+const env = require("dotenv").config()
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUERBASEURL
+};
+
+router.use(auth(config));
+
+router.get('/checkloginstatus', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
 
 router.use("/states", require("./states"));
 router.use("/nationalparks", require("./nationalparks"));
-
 
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
