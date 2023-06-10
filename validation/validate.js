cf7ba6bb0e7a76636b6326c57831822a736562e5
Bentleyr98 <bentley.rebekah314@gmail.com>
2023-06-09 23:35:19 -0600

    fixed validation

--- validation/validate.js
+++ validation/validate.js
@@ -38,14 +38,49 @@ validate.checkData = async (req, res, next) => {
       .trim()
       .escape()
       .isLength({ min: 4 })
-      .withMessage("A valid state is required."),
+      .withMessage("A valid national park is required."),
 
       // valid national parks are required
-      body("nationalParks")
+      body("location")
       .trim()
       .escape()
       .isLength({ min: 1 })
-      .withMessage("A valid national park is required.")
+      .withMessage("A valid location is required."),
+
+      // valid national parks are required
+      body("established_date")
+      .trim()
+      .escape()
+      .isLength({ min: 1 })
+      .withMessage("A valid date is required."),
+
+      // valid national parks are required
+      body("area")
+      .trim()
+      .escape()
+      .isLength({ min: 1 })
+      .withMessage("A valid area is required."),
+
+      // valid national parks are required
+      body("description")
+      .trim()
+      .escape()
+      .isLength({ min: 1 })
+      .withMessage("A valid description is required."),
+
+      // valid national parks are required
+      body("activities")
+      .trim()
+      .escape()
+      .isLength({ min: 1 })
+      .withMessage("A valid activities is required."),
+
+      // valid national parks are required
+      body("website")
+      .trim()
+      .escape()
+      .isLength({ min: 1 })
+      .withMessage("A valid website is required.")
     ]
   }
 
